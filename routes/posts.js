9b73b47091d6f331c5ab3eb6a3f94c303be19459
Mohammed Siddique <khanshahid9066@gmail.com>
2025-04-29 20:36:53 +0530

    EXPRESS_CRASH
    CRUD API : creating get app posts, params and limits

---
+++ routes/posts.js
@@ -0,0 +1,36 @@
+import express from 'express';
+
+const router = express.Router();
+
+let posts = [
+    {id : 1, name : 'Post One'},
+    {id : 2, name : 'Post Two'},
+    {id : 3, name : 'Post Three'},
+]
+
+//Get all posts
+router.get('/', (req, res) => {
+    // console.log(req.query)
+    const limit = parseInt(req.query.limit);
+
+    if (!isNaN(limit) && limit > 0) {
+        return res.status(200).json(posts.slice(0, limit));
+    }
+    res.status(200).json(posts);
+})
+
+//Get single posts
+router.get('/:id', (req, res) => {
+    // console.log(req.params.id)
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id == id);
+
+    if (!post) {
+        return res.status(404).json({msg : `A post with id of ${id} was not found`});
+    }
+    res.status(200).json(post);
+})
+
+// module.exports = router;
+export default router;
+
