992fda362f56c9f7cbd13ffff40b94e87beed0d3
Mohammed Siddique <khanshahid9066@gmail.com>
2025-06-11 20:12:34 +0530

    EXPRESS_CRASH
    CRUD API : creating get app posts, params and limits

--- server.js
+++ server.js
@@ -13,8 +13,27 @@ let posts = [
     {id : 3, name : 'Post Three'},
 ]
 
+//Get all posts
 app.get('/api/posts', (req, res) => {
+    // console.log(req.query)
+    const limit = parseInt(req.query.limit);
+
+    if (!isNaN(limit) && limit > 0) {
+        return res.status(200).json(posts.slice(0, limit));
+    }
     res.status(200).json(posts);
+})
+
+//Get single posts
+app.get('/api/posts/:id', (req, res) => {
+    // console.log(req.params.id)
+    const id = parseInt(req.params.id);
+    let post = posts.find((post) => post.id == id);
+
+    if (!post) {
+        return res.status(404).json({msg : `A post with id of ${id} was not found`});
+    }
+    res.status(200).json(post);
 })
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
