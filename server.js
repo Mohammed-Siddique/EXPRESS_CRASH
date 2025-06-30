eb88502a48112dc11b4f0b9aef9d87dacdc3af86
Mohammed Siddique <khanshahid9066@gmail.com>
2025-06-30 19:29:37 +0530

    EXPRESS_CRASH
    CRUD API : initializing express, app.listen, Server running on PORT

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
-
+const path = require('path');
 const port = process.env.PORT || 8080;
 
 const app = express();
+
+//Setup static path
+
+
+let posts = [
+    {id : 1, name : 'Post One'},
+    {id : 2, name : 'Post Two'},
+    {id : 3, name : 'Post Three'},
+]
+
+app.get('/api/posts', (req, res) => {
+    res.status(200).json(posts);
+})
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
