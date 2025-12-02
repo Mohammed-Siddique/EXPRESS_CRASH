1537aeee605f411da677a7d64ab5cd2032ffa5ea
Mohammed Siddique - IT <mohammed.siddique@mgmforgings.com>
2025-12-02 12:03:55 +0530

    EXPRESS_CRASH
    Adding CRUD operations to the framework

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 import express from 'express';
-import path from 'path';
-import posts from './routes/posts.js';
 const port = process.env.PORT || 8000;
+import posts from './routes/routes.js';
 
 const app = express();
 
-//Body parser
-// app.use(express.json);
-// app.use(express.urlencoded({extended: false}));
+//JSON body parser
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
-//Setup static path
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// Routes
 app.use('/api/posts', posts);
-
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
